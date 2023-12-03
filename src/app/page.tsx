6ca5947a197d6f0cbddb50d5ce34d4b9c8bd068c
s0vers <sovers.tonmoy.pandey@g.bracu.ac.bd>
2023-12-03 12:47:53 +0600

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,127 @@
-import Image from 'next/image'
+import Image from "next/image";
+import Link from "next/link";
+import { MdOutlineHomeWork } from "react-icons/md";
+
+const componet = {
+  title: "Single Family",
+  location: "Los Angeles, CA",
+  image: "/home.jpg",
+  invest: "/",
+  optimatized: "Growth",
+  IRR: "15.7%",
+  Est: "5-7yrs",
+};
+
+const balance = {
+  current: "$25,215",
+  newest: {
+    title1: "1124 Los Angles Blvd",
+    value1: "$300",
+    time1: "Today 10:15am",
+    title2: "553 Kingstone",
+    value2: "$400",
+    time2: "Today 10:15am",
+    title3: "Real Extate Fund",
+    value3: "$400",
+    time3: "Today 10:15am",
+  },
+};
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="bg-black h-screen w-full">
+      <section className="border border-gray-400 py-4 rounded-[16px] max-w-[320px]">
+        <div className="px-4 pb-2">
+          <h1 className="uppercase text-2xl text-white font-semibold">
+            {componet.title}
+          </h1>
+          <h2 className=" text-white/50">{componet.location}</h2>
         </div>
-      </div>
+        <div>
+          <Image src={componet.image} alt="home" width={500} height={500} />
+        </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div className="flex justify-between px-4 py-4 text-center">
+          <div>
+            <h3 className="text-white/50 text-sm">Optimized for</h3>
+            <p className=" text-sm  font-semibold text-[#39bb6b]">
+              {componet.optimatized}
+            </p>
+          </div>
+          <div>
+            <h3 className="text-white/50 text-sm">Avg Hist. IRR</h3>
+            <p className="text-sm font-semibold text-[#39bb6b]">
+              {componet.IRR}
+            </p>
+          </div>
+          <div>
+            <h3 className="text-white/50 text-sm">Ext. exit</h3>
+            <p className=" text-sm font-semibold text-[#39bb6b]">
+              {componet.Est}
+            </p>
+          </div>
+        </div>
+        <Link
+          href={componet.invest}
+          className="bg-transparent border hover:border-[#29b47f] uppercase tracking-widest border-gray-400 hover:bg-[#29b47f] text-white py-2 px-4 mx-4 flex rounded-[18px] justify-center text-center font-medium"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          Invest
+        </Link>
+      </section>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <section className="w-full h-full max-w-[600px] max-h-[400px] overflow-y-clip border rounded-[16px] border-gray-400">
+        <div className="py-6 px-4 mx-4 my-4 rounded-[18px] bg-blue-500/25">
+          <h2 className="text-white/60 text-sm">Credit Balance</h2>
+          <h1 className="text-white text-4xl font-medium tracking-wide leading-10 mt-2">
+            {balance.current}
+          </h1>
+        </div>
+        <div className="px-5">
+          <p className="uppercase text-white/50 text-xs">Newest</p>
+          <div className="flex justify-between align-middle items-center">
+            <div className="flex justify-center align-middle items-center gap-2">
+              <div className="rounded-full px-4 py-4 my-4 bg-gray-600/50 text-xl text-[#39bb6b]">
+                <MdOutlineHomeWork />
+              </div>
+              <div>
+                <h2 className="text-white">{balance.newest.title1}</h2>
+                <p className="text-white/50 text-xs">{balance.newest.time1}</p>
+              </div>
+            </div>
+            <div>
+              <h2 className="text-white">{balance.newest.value1}</h2>
+            </div>
+          </div>
+          <div className="flex justify-between align-middle items-center">
+            <div className="flex justify-center align-middle items-center gap-2">
+              <div className="rounded-full px-4 py-4 my-4 bg-gray-600/50 text-xl text-[#39bb6b]">
+                <MdOutlineHomeWork />
+              </div>
+              <div>
+                <h2 className="text-white">{balance.newest.title1}</h2>
+                <p className="text-white/50 text-xs">{balance.newest.time1}</p>
+              </div>
+            </div>
+            <div>
+              <h2 className="text-white">{balance.newest.value1}</h2>
+            </div>
+          </div>
+          <div className="flex justify-between align-middle items-center">
+            <div className="flex justify-center align-middle items-center gap-2">
+              <div className="rounded-full px-4 py-4 my-4 bg-gray-600/50 text-xl text-[#39bb6b]">
+                <MdOutlineHomeWork />
+              </div>
+              <div>
+                <h2 className="text-white">{balance.newest.title1}</h2>
+                <p className="text-white/50 text-xs">{balance.newest.time1}</p>
+              </div>
+            </div>
+            <div>
+              <h2 className="text-white">{balance.newest.value1}</h2>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
-  )
+  );
 }
